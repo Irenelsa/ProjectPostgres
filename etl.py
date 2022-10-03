770d4f21113bf04eaf16a1eb134958da0ad07568
Irene <irene.lopez.sanz@gmail.com>
2022-10-03 13:24:23 +0200

    Modificaciones para pasar los sanity test

--- etl.py
+++ etl.py
@@ -7,11 +7,20 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    '''
+    Process songs file, convert every file row to tuples to insert in each table
+
+            Parameters:
+                    cur: _Cursor
+                    filepath: string that contains song files path
+
+    '''
     # open song file
     df = pd.read_json(filepath, lines=True)
     df = pd.DataFrame(df)   
     
     for data in df.itertuples():
+        '''with every row we have all information to insert both tables, artist and songs'''
         # insert artist record
         artist_data = (data.artist_id, data.artist_name, data.artist_location, data.artist_latitude, data.artist_longitude)
         cur.execute(artist_table_insert, artist_data)
@@ -23,6 +32,14 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    '''
+    Process logs file. From each row, that is NextSong action, you have to insert data in time, user and songplay tables
+
+            Parameters:
+                    cur: _Cursor
+                    filepath: string that contains log files path
+
+    '''
     # open log file
     df = pd.read_json(filepath, lines=True)
     df = pd.DataFrame(df)   
@@ -38,6 +55,7 @@ def process_log_file(cur, filepath):
     column_labels = ['start_time','hour', 'day', 'week', 'month', 'year', 'weekday']
     time_df = pd.DataFrame(data=time_data, columns=column_labels)
 
+    '''Insert every time row getting info from start_time column '''
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
@@ -45,6 +63,7 @@ def process_log_file(cur, filepath):
     user_df = df[['userId','firstName','lastName','gender','level']]
 
     # insert user records
+    '''Insert user table with file data'''
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
@@ -55,20 +74,34 @@ def process_log_file(cur, filepath):
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
+        '''execute select to get song and artist from tables with title, artist name and duration appears in log data'''
         if results:
             songid, artistid = results
-            # insert songplay record
-            songplay_data = (pd.to_datetime(row.ts),row.userId,row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
-            cur.execute(songplay_table_insert, songplay_data)
             print('Found:  ',row.song)
         else:
             songid, artistid = None, None
             #print('Not found:  ',row.song)
 
 
+        # insert songplay record
+        '''Insert logs data, with or without songs and artist'''
+        songplay_data = (pd.to_datetime(row.ts),row.userId,row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
 
 
 def process_data(cur, conn, filepath, func):
+    '''
+    get all json files recursively from filepath and calling process_song_file or process_song_file functions 
+
+            Parameters:
+                    cur: _Cursor
+                    conn: connection
+                    filepath: string that contains files path
+                    func: function name to process files (rocess_song_file or process_song_file)
+
+    '''
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -88,12 +121,16 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    '''sparkifydb Database connection'''
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
+    '''Process first songs files data '''
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    '''After loading songs, you have to load logs '''
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
+    '''close database connection'''
     conn.close()
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,25 +1,12 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays "
-user_table_drop = "DROP TABLE IF EXISTS users "
-song_table_drop = "DROP TABLE IF EXISTS songs "
-artist_table_drop = "DROP TABLE IF EXISTS artists "
-time_table_drop = "DROP TABLE IF EXISTS time "
+songplay_table_drop = "DROP TABLE IF EXISTS songplays; "
+user_table_drop = "DROP TABLE IF EXISTS users; "
+song_table_drop = "DROP TABLE IF EXISTS songs; "
+artist_table_drop = "DROP TABLE IF EXISTS artists; "
+time_table_drop = "DROP TABLE IF EXISTS time; "
 
 # CREATE TABLES
-
-songplay_table_create = (""" 
-CREATE TABLE IF NOT EXISTS songplays ( 
-    songplay_id serial PRIMARY KEY, 
-    start_time timestamp, 
-    user_id int, 
-    level varchar, 
-    song_id varchar, 
-    artist_id varchar, 
-    session_id int, 
-    location varchar, 
-    user_agent varchar);  
-""")
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
@@ -35,8 +22,8 @@ CREATE TABLE IF NOT EXISTS songs (
     song_id varchar PRIMARY KEY NOT NULL,
     title varchar NOT NULL,
     artist_id varchar NOT NULL,
-    year int,
-    duration float);
+    year int NOT NULL,
+    duration float NOT NULL);
 """)
 
 artist_table_create = ("""
@@ -50,13 +37,26 @@ CREATE TABLE IF NOT EXISTS artists (
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time timestamp,
+    start_time timestamp PRIMARY KEY,
     hour int, 
     day int, 
     week int, 
     month int, 
     year int,                      
     weekday int);
+""")
+
+songplay_table_create = (""" 
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id serial PRIMARY KEY, 
+    start_time timestamp NOT NULL,
+    user_id int NOT NULL, 
+    level varchar, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id int, 
+    location varchar, 
+    user_agent varchar);  
 """)
 
 # INSERT RECORDS
@@ -71,7 +71,7 @@ INSERT INTO songplays(
     session_id,
     location,
     user_agent)
-VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s) 
 """)
 
 user_table_insert = """
@@ -121,6 +121,8 @@ INSERT INTO time(
     year,
     weekday)
 VALUES(%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) 
+DO NOTHING
 """)
 
 # FIND SONGS
